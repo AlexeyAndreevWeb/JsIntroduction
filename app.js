0eee1761c5fc961973d69bffce1d89a4e94893c2
Alexey Andreev <lexandrweb@gmail.com>
2024-01-25 20:42:03 +0300

    final

---
+++ app.js
@@ -0,0 +1,13 @@
+const temperatureC1 = 36;
+const temperatureC2 = 38;
+const temperatureC3 = 20;
+const temperatureC4 = 28;
+
+const temperatureConversionCF = (temperature) => {
+  return (9 / 5) * temperature + 32;
+};
+
+console.log(temperatureConversionCF(temperatureC1));
+console.log(temperatureConversionCF(temperatureC2));
+console.log(temperatureConversionCF(temperatureC3));
+console.log(temperatureConversionCF(temperatureC4));
